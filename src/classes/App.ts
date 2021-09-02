a73c22c9de7ae8327b0ad15670ed855043f538ca
Hassan <hassan.alirobleh@gmail.com>
2021-09-02 22:25:59 +0200

    update test

--- src/classes/App.ts
+++ src/classes/App.ts
@@ -10,7 +10,7 @@ import { FacadeHomeCinema } from '../facades/FacadeHomeCinema';
 
 export class App {
 
-    main() {
+    arretFilm() {
 
         // Instanciation de composant
 
@@ -25,13 +25,32 @@ export class App {
 
         const homeCinema: FacadeHomeCinema = new FacadeHomeCinema(amp, tuner, dvd, cd, projecteur, ecran, lumiere, machineAPopcorn);
 
-        homeCinema.regarderFilm("Hôtel du nord");
+        // homeCinema.regarderFilm("Hôtel du nord");
         return homeCinema.arreterFilm();
 
 
     }
 
+    voirFilm() {
+
+        const tuner: Tuner = new Tuner();
+        const cd: LecteurCd = new LecteurCd();
+        const dvd: LecteurDvd = new LecteurDvd()
+        const amp: Amplificateur = new Amplificateur(tuner, cd, dvd);
+        const ecran = new Ecran();
+        const lumiere = new Lumieres;
+        const projecteur = new Projecteur();
+        const machineAPopcorn = new MachineAPopcorn();
+
+        const homeCinema: FacadeHomeCinema = new FacadeHomeCinema(amp, tuner, dvd, cd, projecteur, ecran, lumiere, machineAPopcorn);
+
+        // return homeCinema.cd.jouer()
+        // return homeCinema.arreterFilm();
+        return homeCinema.regarderFilm("Hôtel du nord");
+
+    }
+
 }
 
-const app = new App();
-app.main();
+// const app = new App();
+// app.main();
--- test/facade.test.ts
+++ test/facade.test.ts
@@ -2,9 +2,13 @@ import { App } from './../src/classes/App';
 
 describe('Pattern Façade test', () => {
     
-    it("should be return true", () => {
+    it("should be return C'est la fin du film ...", () => {
         const app = new App()
-        
-        expect(app.main()).toBe("C'est la fin du film ...");
-    })
+        expect(app.arretFilm()).toBe("C'est la fin du film ...");
+    });
+
+    it("should be return Vous allez voir un bon film ...", () => {
+        const app = new App()
+        expect(app.voirFilm()).toBe("Vous allez voir un bon film ...");
+    });
 })
